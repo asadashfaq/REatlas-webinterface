5576e0ad55b62bc106d25a173aedc0d633133fc1
Manila Chaturvedi <manila@progarya.dk>
2013-10-09 23:17:14 +0200

    Map base selector and new cutout option. Also Introducing configuration file.

---
+++ configurations.php
@@ -0,0 +1,26 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+// Main stuff here
+define('START',microtime());
+
+// DB 
+define('DB_SERVER','');
+define('DATABASE','');
+define('DB_USER','');
+
+
+// Users
+
+define('DEFAULT_USER_GROUP','auesg');
+define('DEFAULT_USER','manila');
+
+
+//selection
+define('SELECTION_TOOLBAR',false);
+define('SINGLE_SELECT',true);
